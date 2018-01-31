dac4c82e74cb1919e7ee4816ef6d290f2f46db89
Fabrice Daugan <fabrice.daugan@gfi.fr>
2018-01-31 11:59:48 +0100

    Update API to 2.1.0

--- src/test/java/org/ligoj/app/plugin/sso/salt/resource/SaltedAuthenticationResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/sso/salt/resource/SaltedAuthenticationResourceTest.java
@@ -10,10 +10,10 @@ import javax.transaction.Transactional;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.ligoj.app.AbstractAppTest;
 import org.ligoj.app.iam.IUserRepository;
 import org.ligoj.app.iam.IamConfiguration;
@@ -23,12 +23,12 @@ import org.mockito.Mockito;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Test class of {@link SaltedAuthenticationResource}
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
 @Transactional
 @Rollback
@@ -37,7 +37,7 @@ public class SaltedAuthenticationResourceTest extends AbstractAppTest {
 	private SaltedAuthenticationResource resource;
 	private IUserRepository userRepository;
 
-	@Before
+	@BeforeEach
 	public void prepareData() throws IOException {
 		// Only with Spring context
 		persistEntities("csv", new Class[] { SystemConfiguration.class }, StandardCharsets.UTF_8.name());
@@ -54,47 +54,55 @@ public class SaltedAuthenticationResourceTest extends AbstractAppTest {
 	@Test
 	public void testGetToken() throws Exception {
 		Mockito.when(userRepository.getToken("jdupont")).thenReturn("pwd");
-		Assert.assertNotNull(StringUtils.trimToNull(resource.getSsoToken("jdupont")));
+		Assertions.assertNotNull(StringUtils.trimToNull(resource.getSsoToken("jdupont")));
 	}
 
 	@Test
 	public void testGetTokenFromUser() throws Exception {
 		Mockito.when(userRepository.getToken("any")).thenReturn(null);
 		Mockito.when(userRepository.getToken(null)).thenReturn(null);
-		Assert.assertNull(resource.getSsoToken("any"));
-		Assert.assertNull(resource.getSsoToken(null));
+		Assertions.assertNull(resource.getSsoToken("any"));
+		Assertions.assertNull(resource.getSsoToken(null));
 	}
 
-	@Test(expected = AccessDeniedException.class)
+	@Test
 	public void testCheckEmptyToken() throws Exception {
-		Assert.assertNull(resource.checkSsoToken(null));
-		Assert.assertNull(resource.checkSsoToken(""));
+		Assertions.assertNull(resource.checkSsoToken(null));
+		Assertions.assertThrows(AccessDeniedException.class, () -> {
+			Assertions.assertNull(resource.checkSsoToken(""));
+		});
 	}
 
 	@Test
 	public void testValidToken() throws Exception {
 		Mockito.when(userRepository.getToken("jdupont")).thenReturn("pwd");
-		Assert.assertEquals("jdupont", resource.checkSsoToken(resource.getSsoToken("jdupont")));
+		Assertions.assertEquals("jdupont", resource.checkSsoToken(resource.getSsoToken("jdupont")));
 	}
 
-	@Test(expected = AccessDeniedException.class)
+	@Test
 	public void checkSsoTokenPasswordChanged() throws Exception {
 		Mockito.when(userRepository.getToken("hdurant")).thenReturn("old-pwd", "new-pwd");
 		final String token = resource.getSsoToken("hdurant");
-		resource.checkSsoToken(token);
+		Assertions.assertThrows(AccessDeniedException.class, () -> {
+			resource.checkSsoToken(token);
+		});
 	}
 
-	@Test(expected = AccessDeniedException.class)
+	@Test
 	public void checkSsoTokenTooOldToken() throws Exception {
 		Mockito.when(userRepository.getToken("mmartin")).thenReturn("pwd");
 		final String token = getOldSsoToken("mmartin", "pwd");
-		resource.checkSsoToken(token);
+		Assertions.assertThrows(AccessDeniedException.class, () -> {
+			resource.checkSsoToken(token);
+		});
 	}
 
-	@Test(expected = AccessDeniedException.class)
+	@Test
 	public void testNotExist() throws Exception {
 		Mockito.when(userRepository.getToken("jdoe4")).thenReturn(null);
-		resource.checkSsoToken(null);
+		Assertions.assertThrows(AccessDeniedException.class, () -> {
+			resource.checkSsoToken(null);
+		});
 	}
 
 	/**
@@ -119,12 +127,13 @@ public class SaltedAuthenticationResourceTest extends AbstractAppTest {
 		final long expire = System.currentTimeMillis() - DateUtils.MILLIS_PER_MINUTE * 31;
 
 		// Generated an encrypted key, valid for 30 minutes
-		return resource.encrypt(login + "|" + sDigest + "|" + sSalt + "|"
-				+ new String(Base64.encodeInteger(new BigInteger(String.valueOf(expire)))), "secret");
+		return resource.encrypt(
+				login + "|" + sDigest + "|" + sSalt + "|" + new String(Base64.encodeInteger(new BigInteger(String.valueOf(expire)))),
+				"secret");
 	}
 
 	@Test
 	public void getKey() {
-		Assert.assertEquals("feature:sso:salt", resource.getKey());
+		Assertions.assertEquals("feature:sso:salt", resource.getKey());
 	}
 }
